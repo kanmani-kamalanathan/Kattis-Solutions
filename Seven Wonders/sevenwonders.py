35c245df3fbd0f1b9fda3cdde03229f33701a11e
Kanmani Kamalanathan <73705050+kanmani-kamalanathan@users.noreply.github.com>
2020-10-31 16:54:24 +0530

    Create sevenwonders.py

---
+++ Seven Wonders/sevenwonders.py
@@ -0,0 +1,7 @@
+#https://open.kattis.com/problems/sevenwonders
+
+cards = input()
+scores = {'T':0, 'C':0, 'G':0}
+for card in cards:
+    scores[card] += 1
+print(sum([ x**2 for x in scores.values()]) + 7 * min(scores.values()))
