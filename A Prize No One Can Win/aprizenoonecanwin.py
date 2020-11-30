962d7d1f7c2ac6979507b2e6fc1e69e73f4cc244
Kanmani Kamalanathan <73705050+kanmani-kamalanathan@users.noreply.github.com>
2020-11-30 19:50:12 +0530

    Create aprizenoonecanwin.py

---
+++ A Prize No One Can Win/aprizenoonecanwin.py
@@ -0,0 +1,11 @@
+n,x = map(int,input().split())
+items = sorted(map(int,input().split()))
+if n == 1:
+    print('1')
+elif items[-1] + items[-2] <= x:
+    print(n)
+else:
+    for i,c in enumerate(items):
+        if c + items[i+1] > x:
+            print(i+1)
+            break
