83047f1230543dbe7d8ec36eeb0d6c21bcc0e49d
Kanmani Kamalanathan <73705050+kanmani-kamalanathan@users.noreply.github.com>
2020-11-30 20:03:45 +0530

    Create intergalacticbidding.py

---
+++ Intergalactic Bidding/intergalacticbidding.py
@@ -0,0 +1,7 @@
+n,s=list(map(int,input().split()))
+wins=[]
+for x,name in sorted(((lambda a,b:(int(b),a))(*input().split()) for _ in range(n)), reverse=True):
+    if s>=x:
+        wins.append(name)
+        s-=x
+print("0" if s else (lambda a,b: f'{a}\n{b}')(*(lambda w:(len(w),'\n'.join(w)))(wins)))
