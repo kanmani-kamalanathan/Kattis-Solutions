687e812301ff129e68a0c5dc84a2b25e7038e069
Kanmani Kamalanathan <73705050+kanmani-kamalanathan@users.noreply.github.com>
2020-11-30 19:57:58 +0530

    Create awkwardparty.py

---
+++ Awkward Party/awkwardparty.py
@@ -0,0 +1,9 @@
+d = {}
+most = int(input())
+for i, l in enumerate(map(int,input().split())):
+    if l not in d:
+        d[l] = i
+    else:
+        most = min(most, i - d[l])
+        d[l] = i
+print(most)
